adf99b794b079ebdd390aa2ffd4d17109d3aba18
Magooch <crvallecito@gmail.com>
2025-07-18 17:37:50 -0600

    Create skeleton to check user input

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let input = document.getElementById("input");
 let output = document.getElementById("output");
-let convert = document.getElementById("btn");
+let convert = document.getElementById("btn"); 
 
-function{convert onclick(
-    output.textContent = "Test complete";
-)}
+convert.addEventListener("click", function(){
+    if (input.value.length > 0) {
+        if (input.value.includes(".")) {
+            output.textContent = "Test complete"
+            input.value = ""
+        } else {
+            output.textContent = "Not a valid IP address"
+            input.value == ""
+        }
+    } else {
+        output.textContent = "Error: No input detected!"
+    }
+})
