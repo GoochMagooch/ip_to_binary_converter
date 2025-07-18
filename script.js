8140d88191391cff26649e6aeb1dd1a3156b61ca
Magooch <crvallecito@gmail.com>
2025-07-18 17:06:01 -0600

    Try to apply test function

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ let input = document.getElementById("input");
 let output = document.getElementById("output");
 let convert = document.getElementById("btn");
 
+function{convert onclick(
+    output.textContent = "Test complete";
+)}
