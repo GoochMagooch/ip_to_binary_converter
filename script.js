b4d984df761b3140b45a941d4d2910bdaa20042b
Magooch <crvallecito@gmail.com>
2025-07-18 18:02:34 -0600

    Split user input by each period

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let convert = document.getElementById("btn");
 convert.addEventListener("click", function(){
     if (input.value.length > 0) {
         if (input.value.includes(".")) {
-            output.textContent = "Test complete"
+            output.textContent = input.value.split(".")
             input.value = ""
         } else {
             output.textContent = "Not a valid IP address"
