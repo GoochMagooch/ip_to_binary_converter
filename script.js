3a9d9288451f32aafef4719d06173b9719ecc1ee
Magooch <crvallecito@gmail.com>
2025-07-18 18:41:56 -0600

    Check for valid IP addresses

--- script.js
+++ script.js
@@ -3,15 +3,42 @@ let output = document.getElementById("output");
 let convert = document.getElementById("btn"); 
 
 convert.addEventListener("click", function(){
+
+    // checks if input contains any value
     if (input.value.length > 0) {
+        // checks if input contains a period for valid IP checks
         if (input.value.includes(".")) {
-            output.textContent = input.value.split(".")
-            input.value = ""
+            octets = input.value.split(".")
+            // checks if an IP contains 4 octets
+            if (octets.length == 4) {
+                let validOctet = true;
+                // validates octets are 0 - 255
+                for (let i = 0; i < octets.length; i++) {
+                    if (octets[i] > 255 || octets[i] < 0) {
+                        validOctet = false;
+                        break;
+                    }
+                }
+                // output depending on valid/invalid octets
+                if (validOctet == false) {
+                    output.textContent = "Invalid octet"
+                } else {
+                    output.textContent = "Valid IP address"
+                }
+            } else {
+                output.textContent = "Not a valid IP address!"
+                input.value = ""
+            }
         } else {
-            output.textContent = "Not a valid IP address"
+            output.textContent = "Not a valid IP address!"
             input.value == ""
         }
     } else {
         output.textContent = "Error: No input detected!"
     }
+
 })
+
+// Notes
+// check octets for non integers
+// check input for only periods or 1 period
